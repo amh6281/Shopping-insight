1217fb2f9ba93ab0bbb552204e13d9d81b903d96
amh6281 <amh6281@naver.com>
2023-07-08 12:41:41 +0900

    Chart 생성 2022.07.08
    recharts library를 사용하여 chart를 구현한다.

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,97 @@
-import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { styled } from "styled-components";
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 2200,
+    amt: 2200,
+  },
+
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 2100,
+    amt: 2100,
+  },
+
+  {
+    name: "Page D",
+    uv: 4500,
+    pv: 2500,
+    amt: 2500,
+  },
+  {
+    name: "Page E",
+    uv: 3200,
+    pv: 2300,
+    amt: 2300,
+  },
+];
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Chart = () => {
-  return <div>Chart</div>;
+  return (
+    <Container>
+      <AreaChart
+        width={730}
+        height={250}
+        data={data}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="uv"
+          stroke="#8884d8"
+          fillOpacity={1}
+          fill="url(#colorUv"
+        />
+        <Area
+          type="monotone"
+          dataKey="pv"
+          stroke="#82ca9d"
+          fillOpacity={1}
+          fill="url(#colorPv"
+        />
+      </AreaChart>
+    </Container>
+  );
 };
 
 export default Chart;
